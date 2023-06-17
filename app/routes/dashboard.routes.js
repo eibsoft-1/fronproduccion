1bd50152ad2bd47bbf832f4ad8dc431449299da0
elingenieroprofe <65376162+elingenieroprofe@users.noreply.github.com>
2023-06-17 11:56:13 -0500

    colocar en minuscala id

--- app/routes/dashboard.routes.js
+++ app/routes/dashboard.routes.js
@@ -43,7 +43,7 @@ dash.get("/usuario", async (req, res)=>{
                 .then(response => response.json())
                 .then(data => {
                     console.log(data);
-                    datos = data[0];
+                    datos = data;
                 })
                 .catch(err => console.error("Error en peticion: " + err))
 
